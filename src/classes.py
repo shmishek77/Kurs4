4d377603ad6e3d9e3f9b801810f89ea9dd94867a
Алексей <shmishek77@gmail.com>
2024-04-02 02:42:53 +0300

    Edit classes

--- src/classes.py
+++ src/classes.py
@@ -3,24 +3,35 @@ import requests
 
 
 class AbstractAPI(ABC):
+    """
+    абстрактный класс для работы с API сервиса с вакансиями
+    """
 
     @abstractmethod
     def get_vacancies(self, text):
         pass
 
     @abstractmethod
-    def filter_vacancies(self):
+    def filter_vacancies(self, text):
         pass
 
 class HeadHunter(AbstractAPI):
+    """
+    Подключается к API и получает вакансии по ключевому слову
+    """
 
     def get_vacancies(self, text):
+        """
+        Получает вакансии по ключевому слову из API сервиса поиска вакансий
+        """
         params = {"text": text, 'per_page': 20, "only_with_salary": "true"}
         vacancies = requests.get("https://api.hh.ru/vacancies", params=params).json()
-        #print(vacancies)
         return vacancies["items"]
 
     def filter_vacancies(self, text):
+        """
+        Выбирает необходтмые значения из полученного списка вакансий и добавляет в словарь vacancies_filter
+        """
         vacancies = self.get_vacancies(text)
         vacancies_filter = []
         for vacancy in vacancies:
@@ -32,9 +43,6 @@ class HeadHunter(AbstractAPI):
                 vacancy['snippet']['requirement'] = "".join(vacancy['snippet']['requirement'].split("<highlighttext>"))
             if "</highlighttext>" in vacancy['snippet']['requirement']:
                 vacancy['snippet']['requirement'] = "".join(vacancy['snippet']['requirement'].split("</highlighttext>"))
-            # if vacancy["salary"] == None:
-            #     vacancy["salary"]["from"] = 0
-            #     vacancy["salary"]["to"] = 0
             if vacancy["salary"]["from"] == None:
                 vacancy["salary"]["from"] = 0
             if vacancy["salary"]["to"] == None:
@@ -52,6 +60,3 @@ class HeadHunter(AbstractAPI):
             })
         return vacancies_filter
 
-
-hh = HeadHunter()
-#print(hh.filter_vacancies("python"))
